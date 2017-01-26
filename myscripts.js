83ef537d5f5907fa3b84e4897921447be80e3c8b
juhnkim419 <juhnkim419@gmail.com>
2017-01-25 23:41:18 -0500

    test

--- myscripts.js
+++ myscripts.js
@@ -1,12 +1,15 @@
 var operand1;
 var operand2;
+
 function getNumbers(){
+  alert("check");
   operand1 = Math.floor(Math.random()*50)+1;  
   operand2 = Math.floor(Math.random()*50)+1;  
   document.getElementById("operand1"),innerHTML=operand1;
   document.getElementById("operand2"),innerHTML=operand2;
   document.getElementById("userAnswer"),innerHTML="";
 }
+
 function checkAnswer(){
   var userSum = document.getElementById("userAnswer").value;
   var sum = operand1 + operand2;
