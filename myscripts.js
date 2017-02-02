d076ff6c9a1e7da0cef54a8993a743edd3d5cb7d
juhnkim419 <juhnkim419@gmail.com>
2017-02-01 23:14:53 -0500

    js scripts

--- myscripts.js
+++ myscripts.js
@@ -20,3 +20,22 @@ function checkAnswer(){
     document.getElementById("results").innerHTML="Wrong!";
   }
 }
+var pics=["http://animalsbreeds.com/wp-content/uploads/2015/11/Korean-Jindo-2.jpg",
+          "https://s-media-cache-ak0.pinimg.com/originals/47/ef/eb/47efeb2d7cfe615822a41b8f5de0935f.jpg",
+         "http://puppytoob.toobnetwork.com/wp-content/uploads/sites/3/2011/09/puppy-pictures15.jpg"];
+var picIndex=0
+function goLeft(){
+  if(picIndex>0){
+    document.getElementById("pic").src=pic[picIndex];
+    picIndex=picIndex-1;
+    console.log(picIndex);
+  }
+}
+
+function goRight(){
+  if(picIndex<(pics.length-1)){
+    document.getElementById("pic").src=pic[picIndex];
+    picIndex=picIndex+1;
+    console.log(picIndex);
+  }
+}
