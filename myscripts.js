fa908fcd82378b82d9ce4036d180d20bb831f6d6
juhnkim419 <juhnkim419@gmail.com>
2017-02-01 23:28:15 -0500

    images

--- myscripts.js
+++ myscripts.js
@@ -20,22 +20,25 @@ function checkAnswer(){
     document.getElementById("results").innerHTML="Wrong!";
   }
 }
-var pics=["http://animalsbreeds.com/wp-content/uploads/2015/11/Korean-Jindo-2.jpg",
-          "https://s-media-cache-ak0.pinimg.com/originals/47/ef/eb/47efeb2d7cfe615822a41b8f5de0935f.jpg",
+var pics=["https://s-media-cache-ak0.pinimg.com/originals/47/ef/eb/47efeb2d7cfe615822a41b8f5de0935f.jpg",
+         "http://animalsbreeds.com/wp-content/uploads/2015/11/Korean-Jindo-2.jpg",
+         "https://s-media-cache-ak0.pinimg.com/564x/2c/50/16/2c50168e43f9215ebc08ac6838d10c79.jpg",
          "http://puppytoob.toobnetwork.com/wp-content/uploads/sites/3/2011/09/puppy-pictures15.jpg"];
 var picIndex=0
 function goLeft(){
   if(picIndex>0){
-    document.getElementById("pic").src=pic[picIndex];
     picIndex=picIndex-1;
+    document.getElementById("pic").src=pics[picIndex];
+    
     console.log(picIndex);
   }
 }
 
 function goRight(){
   if(picIndex<(pics.length-1)){
-    document.getElementById("pic").src=pic[picIndex];
     picIndex=picIndex+1;
+    document.getElementById("pic").src=pics[picIndex];
+    
     console.log(picIndex);
   }
 }
