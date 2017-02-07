4ee115653a5d5da7f77ed3732222d19144fda520
juhnkim419 <juhnkim419@gmail.com>
2017-02-06 23:42:00 -0500

    addpics

--- myscripts.js
+++ myscripts.js
@@ -23,7 +23,11 @@ function checkAnswer(){
 var pics=["https://s-media-cache-ak0.pinimg.com/originals/47/ef/eb/47efeb2d7cfe615822a41b8f5de0935f.jpg",
          "http://animalsbreeds.com/wp-content/uploads/2015/11/Korean-Jindo-2.jpg",
          "https://s-media-cache-ak0.pinimg.com/564x/2c/50/16/2c50168e43f9215ebc08ac6838d10c79.jpg",
-         "http://puppytoob.toobnetwork.com/wp-content/uploads/sites/3/2011/09/puppy-pictures15.jpg"];
+         "http://puppytoob.toobnetwork.com/wp-content/uploads/sites/3/2011/09/puppy-pictures15.jpg",
+         "http://www.thegreatcourses.com/media/catalog/product/cache/1/image/800x600/0f396e8a55728e79b48334e699243c07/1/8/1884---base_image_4.1424267632.jpg",
+         "http://wallpaper-gallery.net/images/hd-images/hd-images-7.jpg",
+         "https://s-media-cache-ak0.pinimg.com/originals/1d/c6/af/1dc6afeac34de537cdfb323be99509d5.jpg",
+         "https://s-media-cache-ak0.pinimg.com/736x/67/63/06/676306aad47f70e74621a822722864b2.jpg"];
 var picIndex=0
 function goLeft(){
   if(picIndex>0){
