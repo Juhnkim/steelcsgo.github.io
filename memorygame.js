ddad3371d05112b82030b156330e4ef35edc6779
juhnkim419 <juhnkim419@gmail.com>
2017-03-22 23:37:05 -0400

    no

--- memorygame.js
+++ memorygame.js
@@ -1,3 +1,18 @@
+function startGame(){
+  placeImg();
+}
+
 function makeMove(id){
   document.getElementById(id +"img").src="";
+}
+
+function getRandomNum(){
+  return Math.floor((Math.random()*6)+1);
+}
+function placeImg(){
+  var location1="row"+getRandomNum()+"col"+getRandomNum()+"img";
+  var location2="row"+getRandomNum()+"col"+getRandomNum()+"img";
+  alert(location1);
+  
+  document.getElementById(location1).src="images/Coral.jpg";
 }
