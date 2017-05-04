699a514dac270c7b39d4c3835458120ac897e6f3
juhnkim419 <juhnkim419@gmail.com>
2017-05-03 23:12:22 -0400

    images

--- memorygame.js
+++ memorygame.js
@@ -1,12 +1,12 @@
 function startGame(){
-  randomizeImages(1);
+  randomizeImages(getRandomNum(10,1));
   placeImg();
 }
 
 function makeMove(id){
   document.getElementById(id +"img").src="";
 }
-
+var randomLoc=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 function getRandomNum(max, min){
   return Math.floor((Math.random()*max)*min);
 }
@@ -31,28 +31,37 @@ var pics=["images/Boats.jpg",
           "images/Wave.jpg"
          ];
 
-var randomLoc=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+var randomLoc=["","","","","","","","","","","","","","","","","",""]
 
 function randomizeImages(mixCount){
   for(var i=0; i<mixCount; i++){
-  var num=getRandomNum(pics.length-1,0);
+  var num=getRandomNum(17,0);
   var temp=pics[0]
   pics[0]=pics[num];
   pics[num]=temp;
+    for(var j=0; j<18; j++){
+      randomLoc[j]=pics[j];
+    }
   }
 }
 function placeImg(){
   //var location1="row"+getRandomNum(6,1)+"col"+getRandomNum()+"img";
   //var location2="row"+getRandomNum(6,1)+"col"+getRandomNum()+"img";
   //alert(location1);
-  for(var h=0; h<1; h++){
-  for(var i=0; i<pics.length; i++){
-    for(var j=0; j<pics.length; j++){
+  
+  var h=0;
+  for(var i=0; i<6; i++){
+    for(var j=0; j<6; j++){
       
-      document.getElementById("row"+ (i +1)+"col"+(j+1)+"img").src=pics[i]
+      document.getElementById("row"+ (j +1)+"col"+(i+1)+"img").src=randomLoc[h];
+      h++;
   
   //document.getElementById(location1).src="images/Coral.jpg";
       }
+    if(h==18){
+       h=0;
+      randomizeImages(getRandomNum(5,1));
+       }
     }
-  }
+  
 }
